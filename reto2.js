a60e9d82f5bcb8a257354990146ad99e554080e6
Judith <73234106+Nimerys@users.noreply.github.com>
2022-12-04 16:49:50 +0100

    Update reto2.js

--- reto2.js
+++ reto2.js
@@ -1,3 +1,12 @@
+const year = 2022
+const holidays = ['01/06', '04/01', '12/25'] // formato MM/DD
+
+// 01/06 es el 6 de enero, jueves. Cuenta.
+// 04/01 es el 1 de abril, un viernes. Cuenta.
+// 12/25 es el 25 de diciembre, un domingo. No cuenta.
+
+countHours(year, holidays) // 2 días -> 4 horas extra en el año
+
 function countHours(year, holidays) {
     let completeDay;
     let dayOfWeek;
