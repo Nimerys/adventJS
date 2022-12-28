0863feecf44a74cf590aa050f70c72bcc48701b1
Judith <73234106+Nimerys@users.noreply.github.com>
2022-12-28 23:59:55 +0100

    reto 12

--- 2022/reto 11/reto11.js
+++ 2022/reto 11/reto11.js
@@ -14,9 +14,9 @@ function getCompleted(part, total) {
     const MCD = (a, b) => {
       let c;
       while (b) {
-        c = b
-        b = a % b
-        a = c
+        c = b;
+        b = a % b;
+        a = c;
       }
       return a
     }
@@ -24,12 +24,12 @@ function getCompleted(part, total) {
     part = part.split(":");
     total = total.split(":");
   
-    let partRed = +part[0] * 3600 + +part[1] * 60 + +part[2]
-    let totalRed = +total[0] * 3600 + +total[1] * 60 + +total[2]
-    const MCDof = MCD(partRed, totalRed)
+    let partRed = +part[0] * 3600 + +part[1] * 60 + +part[2];
+    let totalRed = +total[0] * 3600 + +total[1] * 60 + +total[2];
+    const MCDof = MCD(partRed, totalRed);
   
-    const partEnd = partRed / MCDof
-    const totalEnd = totalRed / MCDof
+    const partEnd = partRed / MCDof;
+    const totalEnd = totalRed / MCDof;
   
-    return partEnd + "/" + totalEnd
+    return partEnd + "/" + totalEnd;
   }
